3c78a714b41e1673b6de19670058ab73088f2d75
Ahmad-M-N <86849808+Ahmad-M-N@users.noreply.github.com>
2024-05-30 13:03:47 -0400

    Uploading the app sub-folder of the Rails app
    Uploading the entire Rails app is too much for a single upload, so uploading the Rails app in parts

---
+++ sample_app/app/helpers/sessions_helper.rb
@@ -0,0 +1,47 @@
+module SessionsHelper
+  
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # Remembers the user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # Forgets the user
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  # Retrives the current logged-in user form database
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user ||= User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in(user)
+        @current_user = user
+      end
+    end
+  end
+  
+  # Checks if a user is logged in
+  def logged_in?
+    return !current_user.nil?
+  end
+  
+  # Logs the user out
+  def log_out
+    forget current_user
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+end
